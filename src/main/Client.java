40ed9057ced68193515e3417d40875b3eb1fa194
vedantpanda <ved07panda@gmail.com>
2025-04-30 02:13:16 +0530

    Vending Machine LLD

--- src/main/Client.java
+++ src/main/Client.java
@@ -1,7 +1,19 @@
 package main;
 
+import main.entities.VendingMachine;
+import main.state.VendingMachineState;
+
 public class Client {
     public static void main(String[] args) {
-
+        try{
+            VendingMachine vendingMachine = VendingMachine.getVendingMachineInstance();
+            VendingMachineState vendingMachineState = vendingMachine.getCurrentState();
+            vendingMachineState.insertMoney(3);
+            vendingMachineState.selectProduct(1);
+            vendingMachineState.dispenseProduct();
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
